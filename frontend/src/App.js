e0db91a2649312592e845a0a7fbc33bb1c400d58
aloniewski2 <aloniewski635@gmail.com>
2025-09-05 13:40:19 -0400

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>DineValley: Restaurant Recommendator</h1>
+      <p>Welcome to your new app! More features coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
